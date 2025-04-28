44beff9937bcae4a54dc355df9e0d43f9c4f5998
attachemd <mattache.vivace@gmail.com>
2025-04-28 15:33:32 +0100

    updated

--- src/app/ft-theme.preset.ts
+++ src/app/ft-theme.preset.ts
@@ -5,18 +5,60 @@ export const FTThemePreset = definePreset(Aura, {
   //Your customizations, see the following sections for examples
   semantic: {
     colorScheme: {
-      light: {
+      root: {
         formField: {
           hoverBorderColor: '{primary.color}'
+        },
+        inputtext: {
+          filled: {
+            background: 'green'
+          }
         }
       },
+      // light: {
+      //   formField: {
+      //     hoverBorderColor: '{primary.color}'
+      //   },
+      //   inputtext: {
+      //     filled: {
+      //       background: 'green'
+      //     }
+      //   }
+      // },
       dark: {
         formField: {
           hoverBorderColor: '{primary.color}'
+        },
+        inputtext: {
+          filled: {
+            background: 'red'
+          }
         }
       }
     }
   },
+
+  // Define color scheme specific tokens or component styles
+  // colorScheme: {
+  //   light: {
+  //     components: {
+  //       inputtext: {
+  //         filled: {
+  //           background: 'red' // Example light mode background using a surface token
+  //         }
+  //       }
+  //     }
+  //   },
+  //   dark: {
+  //     components: {
+  //       inputtext: {
+  //         filled: {
+  //           background: 'green' // Example dark mode background using a darker surface token
+  //         }
+  //       }
+  //     }
+  //   }
+  // },
 
     // components: {
     //   inputtext: {
@@ -24,16 +66,16 @@ export const FTThemePreset = definePreset(Aura, {
     //   }
     // }
 
-
-    variables: {
-
-        inputtext: {
-          border: {
-            radius: '800px'
-          }
-        }
-
+  // inputtext.filled.background
+  components: {
+    inputtext: {
+      root: {
+        borderRadius: 'var(--radius-xl)', // Example: Using a semantic token
+        // Or a specific value:
+        // borderRadius: '10px'
+      },
     }
+  }
 
 
 });
