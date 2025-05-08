6406ab0dc9a7fb3c17d31884b87100b18d3e9d84
attachemd <mattache.vivace@gmail.com>
2025-05-08 23:44:09 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -66,18 +66,6 @@ export class AppComponent implements AfterViewInit, OnDestroy {
 
   ref: DynamicDialogRef | undefined;
 
-  protected readonly fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
-
-  protected readonly fillerContent = Array.from(
-    {length: 50},
-    () =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-  );
-
   protected readonly isMobile = signal(true);
 
   private readonly _mobileQuery: MediaQueryList;
@@ -86,7 +74,7 @@ export class AppComponent implements AfterViewInit, OnDestroy {
   constructor(private renderer: Renderer2, public dialogService: DialogService, public messageService: MessageService) {
     const media = inject(MediaMatcher);
 
-    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQuery = media.matchMedia('(max-width: 40rem)');
     this.isMobile.set(this._mobileQuery.matches);
     this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
     this._mobileQuery.addEventListener('change', this._mobileQueryListener);
