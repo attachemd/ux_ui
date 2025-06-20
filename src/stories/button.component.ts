75ba70583092a5951fc7067a261c4fa5a7f94975
attachemd <mattache.vivace@gmail.com>
2025-06-20 18:33:37 +0100

    updated

--- src/stories/button.component.ts
+++ src/stories/button.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   template: ` <button
+    clas="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
   type="button"
   (click)="onClick.emit($event)"
   [ngClass]="classes"
