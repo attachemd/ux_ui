1b0bd33459077256b772a97a8a0deaee55707a4d
m.attache <mattache.vivace@gmail.com>
2026-03-08 22:53:52 +0000

    feat(toggle-icon-button): add new toggle-icon-button component and showcase

---
+++ src/stories/Buttons/toggle-icon-button/ft.toggle-icon-button.stories.ts
@@ -0,0 +1,70 @@
+import { FtToggleIconButtonComponent } from './ft.toggle-icon-button.component';
+import type { Meta, StoryObj } from '@storybook/angular';
+
+const meta: Meta<FtToggleIconButtonComponent> = {
+    title: 'Buttons/Toggle Icon Button',
+    component: FtToggleIconButtonComponent,
+    tags: ['autodocs'],
+    argTypes: {
+        color: {
+            control: 'select',
+            options: ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger'],
+        },
+        unselectedColor: {
+            control: 'select',
+            options: ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger'],
+        },
+        selectedColor: {
+            control: 'select',
+            options: ['primary', 'secondary', 'tertiary', 'success', 'warning', 'danger'],
+        },
+        unselectedVariant: {
+            control: 'select',
+            options: ['flat', 'outlined', 'faded', 'ghost'],
+        },
+        selectedVariant: {
+            control: 'select',
+            options: ['flat', 'outlined', 'faded', 'ghost'],
+        },
+        size: {
+            control: 'select',
+            options: ['xs-size', 'sm-size', 'md-size', 'lg-size'],
+        },
+        radius: {
+            control: 'select',
+            options: ['none-radius', 'xs-radius', 'sm-radius', 'md-radius', 'lg-radius', 'full-radius'],
+        },
+        state: {
+            control: 'select',
+            options: ['rest', 'hover', 'press', 'focus', 'disabled'],
+        },
+    },
+    args: {
+        color: 'primary',
+        unselectedColor: undefined,
+        selectedColor: undefined,
+        size: 'md-size',
+        radius: 'md-radius',
+        state: 'rest',
+        iconClass: 'favorite_border',
+        selectedIconClass: 'favorite',
+        selected: false,
+        unselectedVariant: 'ghost',
+        selectedVariant: 'flat'
+    }
+};
+
+export default meta;
+type Story = StoryObj<FtToggleIconButtonComponent>;
+
+export const Default: Story = {
+    args: {
+        selected: false,
+    }
+};
+
+export const Selected: Story = {
+    args: {
+        selected: true,
+    }
+};
